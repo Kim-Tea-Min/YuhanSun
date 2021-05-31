bd4eecddfcff0d5daaa04ceeefd119406913c3b0
kangpunch <swjy333@gmail.com>
2021-05-31 12:36:14 +0900

    변수변경및ui수정

--- Assets/PlusMinus.cs
+++ Assets/PlusMinus.cs
@@ -1,88 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 
 
 public class PlusMinus : MonoBehaviour
 {
-  public UnityEngine.UI.Text mainText;
-  public GameObject MinusButton;
-  public GameObject PlusButton;
+    public TextMeshProUGUI mainText;
+    public GameObject MinusButton;
+    public GameObject PlusButton;
 
-  private int nextnumber;
+    private int score;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    mainText.text = "2";
-    nextnumber = 2;
-  }
-  public void Plus()
-  {
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainText.text = "2";
+        score = 2;
+    }
+    public void Plus()
+    {
 
-    if (int.Parse(mainText.text) > 7)
-    {
-      nextnumber = 8;
-      mainText.text = nextnumber.ToString();
-    }
-    else
-    {
-      nextnumber = int.Parse(mainText.text) + 1;
-      mainText.text = nextnumber.ToString();
-    }
-    Debug.Log(nextnumber);
-  }
+        if (score > 7)
+        {
+            score = 8;
+            mainText.text = score.ToString();
+        }
+        else
+        {
+            score = score + 1;
+            mainText.text = score.ToString();
+        }
 
-  public void Minus()
-  {
-    nextnumber = 0;
-
-    if (int.Parse(mainText.text) < 3)
-    {
-      nextnumber = 2;
-      mainText.text = nextnumber.ToString();
-    }
-    else
-    {
-      nextnumber = int.Parse(mainText.text) - 1;
-      mainText.text = nextnumber.ToString();
-    }
-    Debug.Log(nextnumber);
-  }
-  public void OnClick()
-  {
-    SceneManager.LoadScene("ThirdScene");
-    NumberCounter.numeber = nextnumber;
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    int nextnumber = 0;
-    if (int.Parse(mainText.text) == 2)
-    {
-      nextnumber = 2;
-      mainText.text = nextnumber.ToString();
-      MinusButton.SetActive(false);
-    }
-    else
-    {
-      MinusButton.SetActive(true);
-    }
-    if (int.Parse(mainText.text) == 8)
-    {
-      nextnumber = 8;
-      mainText.text = nextnumber.ToString();
-      PlusButton.SetActive(false);
-    }
-    else
-    {
-      PlusButton.SetActive(true);
     }
 
+    public void Minus()
+    {
+        if (score < 3)
+        {
+            score = 2;
+            mainText.text = score.ToString();
+        }
+        else
+        {
+            score = score - 1;
+            mainText.text = score.ToString();
+        }
 
-  }
+    }
+    public void OnClick()
+    {
+        SceneManager.LoadScene("ThirdScene");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score == 2)
+        {
+            score = 2;
+            mainText.text = score.ToString();
+            MinusButton.SetActive(false);
+        }
+        else
+        {
+            MinusButton.SetActive(true);
+        }
+        if (int.Parse(mainText.text) == 8)
+        {
+            score = 8;
+            mainText.text = score.ToString();
+            PlusButton.SetActive(false);
+        }
+        else
+        {
+            PlusButton.SetActive(true);
+        }
+
+
+    }
 
 }
